94a64b59bcd1d9e78e1c70ce98f0b73b6c3723ff
Farid <farid-f32@msn.com>
2022-07-18 22:07:26 +0200

    Create find_lr.sh

---
+++ find_lr.sh
@@ -0,0 +1,19 @@
+time python -m torch.distributed.launch --nproc_per_node=1 find_lr.py \
+--data_path imagenet/train \
+--epochs 2 \
+--base_lr 0.6 \
+--lr_minx 0.4 \
+--lr_max 0.6 \
+--logspace \
+--final_lr 0.0006 \
+--warmup_epochs 0 \
+--batch_size 32 \
+--dump_path swav_checkpoint \
+--size_crops 224 96 \
+--nmb_crops 2 6 \
+--min_scale_crops 0.14 0.05 \
+--max_scale_crops 1. 0.14 \
+--use_fp16 true \
+--freeze_prototypes_niters 5005 \
+--queue_length 3840 \
+--epoch_queue_starts 15
