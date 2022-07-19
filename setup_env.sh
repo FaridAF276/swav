33d2022fe9adae648cbd3eae64bc81b6d4046078
Farid <farid-f32@msn.com>
2022-07-19 20:35:48 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -9,14 +9,11 @@ apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 li
 wget -nc https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 bash ~/Anaconda3-2022.05-Linux-x86_64.sh -b -p
 cd swav
+conda create -y --name=swav python=3.6.6 pandas=0.25.0 opencv
 source ~/anaconda3/etc/profile.d/conda.sh # Or path to where your conda is
-conda create -y --name=swav python=3.6.6
 conda activate swav
-conda update -y -n base -c defaults conda
 conda install -y pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
 # conda install -y -c conda-forge cudatoolkit-dev=10.1.243
-conda install -y -c conda-forge opencv
-conda install -y -c anaconda pandas=0.25.0
 which pip
 git clone "https://github.com/NVIDIA/apex"
 cd apex
