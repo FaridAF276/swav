a209a086b3d070a9cb877db26726ab1705e26d75
Farid <farid-f32@msn.com>
2022-07-19 20:12:13 +0200

    split work

--- swav_imagenet.sh
+++ swav_imagenet.sh
@@ -1,40 +1,10 @@
-# apt-get install -y git && git clone https://github.com/FaridAF276/swav.git && chmod +x swav/swav_imagenet.sh && bash -e swav/swav_imagenet.sh
-#Setup everything to install apex correctly (configure vast.ai with cuda 10.1 and pytorch 1.4.0)
-wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
-tar -xvf gdrive_2.1.1_linux_386.tar.gz
-./gdrive about
-apt-get install -y unzip zip git wget
-apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
-bash ~/Anaconda3-2022.05-Linux-x86_64.sh -b -p
-cd swav
-source ~/anaconda3/etc/profile.d/conda.sh # Or path to where your conda is
-conda create -y --name=swav python=3.6.6
-conda activate swav
-conda update -y -n base -c defaults conda
-conda install -y pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
-# conda install -y -c conda-forge cudatoolkit-dev=10.1.243
-conda install -y -c conda-forge opencv
-conda install -y -c anaconda pandas=0.25.0
-which pip
-git clone "https://github.com/NVIDIA/apex"
-cd apex
-git checkout 4a1aa97e31ca87514e17c3cd3bbc03f4204579d0
-python setup.py install --cuda_ext
-python -c 'import apex; from apex.parallel import LARC' # should run and return nothing
-python -c 'import apex; from apex.parallel import SyncBatchNorm; print(SyncBatchNorm.__module__)' # should run and return apex.parallel.optimized_sync_batchnorm 
-cd ~/swav/
-conda install -y -c conda-forge gdown
-#install gdrive
-# tail -n +29 swav_imagenet.sh | bash
-
 ##Download dataset
 
 gdown --fuzzy https://drive.google.com/file/d/1_dRbJEpMH7436l8aU4xrGHcFIE9i5TX7/view?usp=sharing
 unzip tiny_imagenet_200.zip
 mkdir swav_checkpoint
 
-time python dataset_preparation.py \
+time python dataset_prep.py \
 --dataset_dir imagenet \
 --percentage 0.2
 
