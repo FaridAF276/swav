f71b65a8e94acaed72f9a0f91dd470e215cfeff4
Farid <farid-f32@msn.com>
2022-07-19 20:12:50 +0200

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
-# apt-get install -y git && git clone https://github.com/FaridAF276/swav.git && chmod +x swav/swav_imagenet.sh && bash -e swav/swav_imagenet.sh
+# apt-get install -y git && git clone https://github.com/FaridAF276/swav.git && chmod +x swav/setup_env.sh && bash -e swav/setup_env.sh
 #Setup everything to install apex correctly (configure vast.ai with cuda 10.1 and pytorch 1.4.0)
 wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 tar -xvf gdrive_2.1.1_linux_386.tar.gz
